594757d1e87a6c64c55eb37e1cb021404353d2b4
HaoThanhIT <thanhhao@playgroundvina.com>
2024-03-11 12:00:51 +0700

    update icon

--- app/src/main/java/flashlight/phichung/com/torch/ui/theme/Color.kt
+++ app/src/main/java/flashlight/phichung/com/torch/ui/theme/Color.kt
@@ -11,7 +11,10 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 
-val PrimaryColor = Color(0xFF313035)
+val PrimaryColor = Color(0xFF191919)
 
 val TextWhiteColor= Color(0xFFFFFFFF)
 val IconWhiteColor= Color(0xFFFFFFFF)
+
+
+val GrayColor= Color(0xFF313035)
