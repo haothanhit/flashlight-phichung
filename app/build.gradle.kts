917b68ace6d60fbe38b299f6d4aa41ecce649ff1
HaoThanhIT <58518334+haothanhit@users.noreply.github.com>
2024-05-15 17:06:02 +0700

    firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,6 +110,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:$appcompatX")
     implementation("com.google.firebase:firebase-analytics:22.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/flashlight/phichung/com/torch/ui/activity/MainActivity.kt
+++ app/src/main/java/flashlight/phichung/com/torch/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package flashlight.phichung.com.torch.ui.activity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 import flashlight.phichung.com.torch.FlashlightApp
 import flashlight.phichung.com.torch.ui.theme.FlashlightTheme
@@ -12,6 +13,10 @@ import flashlight.phichung.com.torch.ui.theme.FlashlightTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Firebase
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
         setContent {
             FlashlightTheme {
                 FlashlightApp()
